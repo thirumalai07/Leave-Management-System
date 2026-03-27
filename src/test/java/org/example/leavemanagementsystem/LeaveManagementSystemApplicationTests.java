e92437b9ec63d6648e5a7e8bed8d2c2e8ee151b5
thirumalai07 <thirumalaitjcs@gmail.com>
2026-03-27 14:12:33 +0530

    Initial commit: Leave Management System project

---
+++ src/test/java/org/example/leavemanagementsystem/LeaveManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.leavemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
